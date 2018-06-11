28c09fef79ba2835a8694cc627dd318008391e85
Alexander Nelson <amvetserv@gmail.com>
2018-06-11 23:47:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,15 +18,10 @@ function currentLine(line) {
   if (line.length === 0) {
     return "The line is currently empty.";
   }
-<<<<<<< HEAD
+
   for (var i = 0; i < line.length; i++) {
     current.push(i + 1 + ". " + line[i]);
   }
   return "The line is currently: " + current.join(', ');
-=======
-  for(var i = 0; i < line.length; i++) {
-    current.push((i + 1). line[i]);
-  }
-  return `The line is currently: ${current.join(', ')}`
->>>>>>> d729d9c149ffb2bd8417e08cc9a5c0eaf20a260e
+
 }
